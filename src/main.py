d4c533d90ced43ef42469c9012ce4800047fe219
Ryan Sakurai <ryansakurai@estudante.ufscar.br>
2024-03-05 20:30:04 -0300

    implement log_in()

--- src/main.py
+++ src/main.py
@@ -1,22 +1,34 @@
 import time
 import pyautogui as gui
 
+def main():
+    open_browser()
+    open_website()
+    log_in()
+
 def open_browser():
     gui.press("win")
     time.sleep(1)
     gui.write("edge")
     gui.press("enter")
+
     time.sleep(3)
 
 def open_website():
     gui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
     gui.press("enter")
+
     time.sleep(3)
 
+def log_in():
+    gui.press("tab")
+    gui.write("mr.employee.man@gmail.com")
+    gui.press("tab")
+    gui.write("strongpassword123")
+    gui.press("tab")
+    gui.press("enter")
 
-def main():
-    open_browser()
-    open_website()
+    time.sleep(3)
 
 
 if __name__ == "__main__":
