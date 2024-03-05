6afa1d2fa214cf7f6b541aba8614ffa52ddfe231
Ryan Sakurai <ryansakurai@estudante.ufscar.br>
2024-03-05 19:57:02 -0300

    add open website

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+import time
 import pyautogui as gui
 
 gui.press("win")
 gui.write("edge")
 gui.press("enter")
+
+time.sleep(3)
+
+gui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+gui.press("enter")
+time.sleep(3)
