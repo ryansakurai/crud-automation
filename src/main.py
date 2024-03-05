9bd550c595732f4afc4a427ad88502b445b5b89c
Ryan Sakurai <ryansakurai@estudante.ufscar.br>
2024-03-05 20:22:38 -0300

    reorganize file

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 import time
 import pyautogui as gui
 
-gui.press("win")
+def open_browser():
+    gui.press("win")
+    time.sleep(1)
+    gui.write("edge")
+    gui.press("enter")
+    time.sleep(3)
 
-time.sleep(1)
+def open_website():
+    gui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+    gui.press("enter")
+    time.sleep(3)
 
-gui.write("edge")
-gui.press("enter")
 
-time.sleep(3)
+def main():
+    open_browser()
+    open_website()
 
-gui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
-gui.press("enter")
-time.sleep(3)
+
+if __name__ == "__main__":
+    main()
